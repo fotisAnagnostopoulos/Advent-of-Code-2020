0c494196d91a4e30428c875bd404e6c735122224
fotis anagnostopoulos <fotis-anagnostopoulos@hotmail.com>
2020-12-13 03:51:58 +0200

    Day-1_complete

---
+++ Day1/part2.py
@@ -0,0 +1,25 @@
+
+
+def parser(input_txt_file):
+    '''
+    Takes a .txt file and returns an array with each elemnt to be that of a line
+    '''
+    with open(input_txt_file,'r') as ff:
+        a = ff.read()
+        blacklist = [',','.','?','!','(',')',';','’','"','--','-',' ','\t']
+    for item in blacklist:
+        a = a.replace(item,'')
+    res = a.split("\n")
+    return res
+
+data = parser("data.txt")
+#brute force - I can not see any possibility for optimization -
+for i in range(len(data)):
+    for j in range(i+1,len(data)):
+        for k in range(j+1,len(data)):
+            suma = int(data[i]) + int(data[j]) + int(data[k]) - 2020
+            # print(suma)
+            if suma == 0:
+                print(int(data[i]) * int(data[j]) * int(data[k]))
+                print(int(data[i]) , int(data[j]) , int(data[k]))
+                break
